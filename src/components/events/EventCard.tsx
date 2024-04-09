8fc69a9f0447c050bad17652c012317124cbd0fc
JeffreyJoel <jowoloko@gmail.com>
2024-04-09 01:03:28 +0100

    add event cards

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -5,7 +5,8 @@ import { UsersRound } from "lucide-react";
 
 export const EventCard = () => {
   return (
-    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow relative">
+        <span className="bg-white rounded-md px-3 py-1 text-xs font-semibold absolute left-3 top-3">Free</span>
       <Image className="rounded-t-lg" src={cardImage} alt="" />
       <div className="p-5 px-3">
         <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900">
@@ -15,10 +16,11 @@ export const EventCard = () => {
           ESG Regulations, Standards and Disclosure: Who to publish it for and
           how to make it meaningful.
         </p>
-        <div className="card-footer flex justify-between mt-8">
+        <div className="card-footer flex justify-between items-center mt-8  border-t pt-3">
           <div>
-            <UsersRound className="text-gray-900" />
+           <span className="flex items-center text-sm"> <UsersRound className="text-gray-900 mr-2 text-xs" /> 20</span>
           </div>
+          <span className="rounded-full bg-green-400 px-3 py-1 text-xs text-white font-semibold">Live</span>
         </div>
       </div>
     </div>
